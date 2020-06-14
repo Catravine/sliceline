961f86e8226ff10cc5b4a687b177d536dfe3bbd3
Caroline Courtney <caroline@carolinecourtney.com>
2020-06-14 14:36:07 -0400

    add navbar and title styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from "styled-components";
+import { Navbar } from "./Navbar/Navbar";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        font-family: 'Open Sans', sans-serif;
+        margin: 0;
+    }
+    
+    h1, h2, h3 {
+        font-family: 'Righteous', cursive;
+    }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <GlobalStyle />
+        <Navbar/>
+        <div>Hello Sliceline</div>
+    </>
   );
 }
 
