dffee7c6355cf1eabf444318a2de272355cef19d
Caroline Courtney <caroline@carolinecourtney.com>
2020-06-21 14:23:08 -0400

    Orders and open food hooks

--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -66,11 +66,21 @@ const DialogBannerName = styled(FoodLabel)`
   padding: 5px 40px; 
 `;
 
-export function FoodDialog({openFood, setOpenFood}) {
+export function FoodDialog({openFood, setOpenFood, setOrders, orders}) {
   function close() {
     setOpenFood();
   }
   if (!openFood) return null;
+
+  const order = {
+    name: openFood.name
+  }
+
+  function addToOrder() {
+    setOrders([...orders, order]);
+    close();
+  }
+
   return (
     <>
       <DialogShadow onClick={close}/>
@@ -82,7 +92,7 @@ export function FoodDialog({openFood, setOpenFood}) {
 
         </DialogContent>
         <DialogFooter>
-          <ConfirmButton>Add to Order</ConfirmButton>
+          <ConfirmButton onClick={addToOrder}>Add to Order</ConfirmButton>
         </DialogFooter>
       </Dialog>
     </>
