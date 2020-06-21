1657a23d5f89ae41124750b58dc35cfe58fce2f1
Caroline Courtney <caroline@carolinecourtney.com>
2020-06-21 15:23:37 -0400

    Added quantities and controls

--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -4,6 +4,8 @@ import {FoodLabel} from "../Menu/FoodGrid";
 import {pizzaRed} from "../Styles/colors";
 import {Title} from "../Styles/title";
 import {formatPrice} from "../Data/FoodData";
+import {QuantityInput} from "./QuantityInput";
+import {useQuantity} from "../Hooks/useQuantity";
 
 const Dialog = styled.div`
   width: 500px;
@@ -30,6 +32,7 @@ const DialogShadow = styled.div`
 export const DialogContent = styled.div`
   min-height: 100px;
   overflow: auto;
+  padding: 0px 40px;
 `;
 
 export const DialogFooter = styled.div`
@@ -67,14 +70,19 @@ const DialogBannerName = styled(FoodLabel)`
   padding: 5px 40px; 
 `;
 
-export function FoodDialog({openFood, setOpenFood, setOrders, orders}) {
+export function getPrice(order) {
+  return order.quantity * order.price;
+}
+
+function FoodDialogContainer({openFood, setOpenFood, setOrders, orders}) {
+  const quantity = useQuantity(openFood && openFood.quantity);
   function close() {
     setOpenFood();
   }
-  if (!openFood) return null;
 
   const order = {
-    ...openFood
+    ...openFood,
+    quantity: quantity.value
   };
 
   function addToOrder() {
@@ -90,14 +98,23 @@ export function FoodDialog({openFood, setOpenFood, setOrders, orders}) {
           <DialogBannerName>{openFood.name}</DialogBannerName>
         </DialogBanner>
         <DialogContent>
-
+          <QuantityInput quantity={quantity}/>
         </DialogContent>
         <DialogFooter>
           <ConfirmButton onClick={addToOrder}>
-            Add to Order: {formatPrice(openFood.price)}
+            Add to Order: {formatPrice(getPrice(order))}
           </ConfirmButton>
         </DialogFooter>
       </Dialog>
     </>
   );
+}
+
+export function FoodDialog(props) {
+  if (!props.openFood) return null;
+  return (
+    <FoodDialogContainer {...props}>
+
+    </FoodDialogContainer>
+  )
 }
