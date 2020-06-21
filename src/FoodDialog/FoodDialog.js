34484c2a945697717ecb15d9c9c59f11ce72b873
Caroline Courtney <caroline@carolinecourtney.com>
2020-06-21 14:36:21 -0400

    add price

--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import {FoodLabel} from "../Menu/FoodGrid";
 import {pizzaRed} from "../Styles/colors";
 import {Title} from "../Styles/title";
+import {formatPrice} from "../Data/FoodData";
 
 const Dialog = styled.div`
   width: 500px;
@@ -73,8 +74,8 @@ export function FoodDialog({openFood, setOpenFood, setOrders, orders}) {
   if (!openFood) return null;
 
   const order = {
-    name: openFood.name
-  }
+    ...openFood
+  };
 
   function addToOrder() {
     setOrders([...orders, order]);
@@ -92,7 +93,9 @@ export function FoodDialog({openFood, setOpenFood, setOrders, orders}) {
 
         </DialogContent>
         <DialogFooter>
-          <ConfirmButton onClick={addToOrder}>Add to Order</ConfirmButton>
+          <ConfirmButton onClick={addToOrder}>
+            Add to Order: {formatPrice(openFood.price)}
+          </ConfirmButton>
         </DialogFooter>
       </Dialog>
     </>
