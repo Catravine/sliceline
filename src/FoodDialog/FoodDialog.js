3676718e79c2f651952f5c2f6f0fe239a6be9977
Caroline Courtney <caroline@carolinecourtney.com>
2020-06-27 19:41:08 -0400

    added toppings to pizzas

--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -6,6 +6,8 @@ import {Title} from "../Styles/title";
 import {formatPrice} from "../Data/FoodData";
 import {QuantityInput} from "./QuantityInput";
 import {useQuantity} from "../Hooks/useQuantity";
+import {Toppings} from "./Toppings";
+import {useToppings} from "../Hooks/useToppings";
 
 const Dialog = styled.div`
   width: 500px;
@@ -33,6 +35,7 @@ export const DialogContent = styled.div`
   min-height: 100px;
   overflow: auto;
   padding: 0px 40px;
+  padding-bottom: 80px;
 `;
 
 export const DialogFooter = styled.div`
@@ -57,7 +60,6 @@ export const ConfirmButton = styled(Title)`
 const DialogBanner = styled.div`
   min-height: 200px;
   margin-bottom: 20px;
-  // background-image: url('/img/pizza2.jpeg');
   ${({img}) => `background-image: url(${img});`}
   background-position: center;
   background-size: cover;
@@ -70,19 +72,27 @@ const DialogBannerName = styled(FoodLabel)`
   padding: 5px 40px; 
 `;
 
+const pricePerTopping = 0.5;
+
 export function getPrice(order) {
-  return order.quantity * order.price;
+  return order.quantity * (order.price + order.toppings.filter(t => t.checked).length * pricePerTopping);
+}
+
+function hasToppings(food) {
+  return food.section  === 'Pizza';
 }
 
 function FoodDialogContainer({openFood, setOpenFood, setOrders, orders}) {
   const quantity = useQuantity(openFood && openFood.quantity);
+  const toppings = useToppings(openFood.toppings);
   function close() {
     setOpenFood();
   }
 
   const order = {
     ...openFood,
-    quantity: quantity.value
+    quantity: quantity.value,
+    toppings: toppings.toppings
   };
 
   function addToOrder() {
@@ -99,6 +109,10 @@ function FoodDialogContainer({openFood, setOpenFood, setOrders, orders}) {
         </DialogBanner>
         <DialogContent>
           <QuantityInput quantity={quantity}/>
+          {hasToppings(openFood) && <>
+            <h3>Would you like toppings?</h3>
+            <Toppings {...toppings} />
+          </>}
         </DialogContent>
         <DialogFooter>
           <ConfirmButton onClick={addToOrder}>
